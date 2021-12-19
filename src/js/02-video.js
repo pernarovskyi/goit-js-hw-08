b32d031901dc1909c5b140986765ec9a1bcb6b5e
Yurii Pernarovskyi <j.pernarovskyi@gmail.com>
2021-12-20 00:59:23 +0200

    Added Task 2 with vimeo player over npm dependencies

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', getVideoCurTime);
+
+function getVideoCurTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+  });
+}
+
+const getTimeFromLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(getTimeFromLocalStorage);
