71d6cf84eaefc4396fe17fd4458200d9b0870821
Yurii Pernarovskyi <j.pernarovskyi@gmail.com>
2021-12-22 22:58:18 +0200

    Fixed bug with video player in Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash/throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle(getVideoCurTime, 1000));
+player.on('timeupdate', throttle(pausedTime, 1000));
 
-function getVideoCurTime() {
+function pausedTime() {
   player.getCurrentTime().then(function (seconds) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
   });
 }
 
-const getTimeFromLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+const getTimeFromLocalStorage = JSON.parse(localStorage.getItem('videoplayer-current-time') || 0);
 
+console.log(getTimeFromLocalStorage);
 player.setCurrentTime(getTimeFromLocalStorage);
